15389b59001076529e4310f67ee0bb884ef624ae
Ricardo Chavelas <ricardo.chavelas.m@gmail.com>
2024-06-16 09:05:15 -0600

    Initial commit (Snippet #3)

---
+++ 3. Template inheritance/app.py
@@ -0,0 +1,15 @@
+"""
+This is a boilerplate for a simple web application in Flask showing how
+template inheritance works
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
