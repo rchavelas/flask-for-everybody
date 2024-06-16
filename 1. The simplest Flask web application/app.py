51c486a9208bd1fa277cc9b0d55e2cfc7261a5ea
Ricardo Chavelas <ricardo.chavelas.m@gmail.com>
2024-06-16 08:48:02 -0600

    Update app.py

--- 1. The simplest Flask web application/app.py
+++ 1. The simplest Flask web application/app.py
@@ -5,13 +5,14 @@ It is recommended to create a virtual environment whenever working
 with a flask application. To do so run in the console: 
     
     'python3 -m venv venv'
+    'venv\Scripts\activate' (if in windows)
 
 Make sure to first install Flask in the console with:
     
     'pip install Flask'
 
 To run the application, run in the console within the file's directory:
-    
+
     'flask run --debug'
 
 """
@@ -24,4 +25,4 @@ def index():
     return 'Hello, World'
 
 if __name__ == '__main__':
-    app.run
+    app.run()
