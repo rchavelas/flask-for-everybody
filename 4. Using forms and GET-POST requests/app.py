3b768b832fe155089a68fdcc3e1942b1c4d140ed
Ricardo Chavelas <ricardo.chavelas.m@gmail.com>
2024-06-16 21:06:56 -0600

    Initial commit (Snippet #4)

---
+++ 4. Using forms and GET-POST requests/app.py
@@ -0,0 +1,27 @@
+"""
+This is a boilerplate for a web application in Flask showing how to
+retrieve (request) information from forms and deal with GET and POST
+requests.
+"""
+
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'very secret and long random key'
+
+# In this case, my_list is just a placeholder for information that 
+# should be stored in a database. For simplicity, we use a simple list
+# to show how to use forms and GET/POST requests 
+my_list = ['1st item', '2nd item']
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        new_item = request.form['new_item']
+        my_list.append(new_item)
+        return redirect(url_for('index'))
+    
+    return render_template('index.html', my_list = my_list)
+
+if __name__ == '__main__':
+    app.run()
