0dea1794309645db573043b39477545b2c75abaa
Ricardo Chavelas <ricardo.chavelas.m@gmail.com>
2024-06-21 16:58:50 -0600

    Initial commit (Snippet #16)

---
+++ 16. User session management with Flask-Login and Flask-SQLAlchemy/app/__init__.py
@@ -0,0 +1,55 @@
+"""
+This is a boilerplate for doing a very simple session management app using 
+Flask-Login and Flask-SQLAlchemy.
+
+Make sure to first install Flask-Login and Flask-SQLAlchemy in the console 
+with:
+    
+    'pip install flask-login flask-sqlalchemy'
+
+For a more traditional tutorial regarding the use of this packages see:
+https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login#step-5-creating-user-models
+
+"""
+
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+DB_NAME = 'db.sqlite'
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'Very secret key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)
+
+    # Registering blueprints
+    from .auth import auth
+    from .main import main
+    app.register_blueprint(auth)
+    app.register_blueprint(main)
+
+    # Create db
+    from .models import User
+    create_database(app)
+
+    # Define the login manager
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def user_loader(id):
+        return db.session.get(User,int(id))
+
+    return app
+
+def create_database(app):
+    with app.app_context():
+        if not os.path.exists(f'instance/{DB_NAME}'):
+            db.create_all()
---
+++ 16. User session management with Flask-Login and Flask-SQLAlchemy/app/auth.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user
+from .models import User
+from . import db
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        # Get form information
+        email = request.form.get('email')
+        password = request.form.get('password')
+        # Validate if user exists
+        user = db.session.scalars(db.select(User).where(User.email==email)).first()
+        if not user or not check_password_hash(user.password, password):
+            return redirect(url_for('auth.login'))
+        # Login user
+        login_user(user)
+        return redirect(url_for('main.profile'))
+    return render_template('login.html')
+
+@auth.route('/signup', methods=['GET','POST'])
+def signup():
+    if request.method == 'POST':
+        # Get form information
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+        # Validate if user already exists
+        user = db.session.scalars(db.select(User).where(User.email==email)).first()
+        if user:
+            return redirect(url_for('auth.login'))
+        # Validate if both passwords subbmited are the same
+        if password != confirm_password:
+            return redirect(url_for('auth.signup'))
+        # Create new user
+        new_user = User(email=email, password=
+                        generate_password_hash(password, 
+                                               method='scrypt'))
+        db.session.add(new_user)
+        db.session.commit()
+        # Redirect to login page
+        return redirect(url_for('auth.login'))    
+    return render_template('signup.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
