155b662cd58ac5f0aab1196871c828a324937d50
Ricardo Chavelas <ricardo.chavelas.m@gmail.com>
2024-06-28 14:14:38 -0600

    Initial commit (Snippet q)

---
+++ q. User session management with Flask-Login, Flask-SQLAlchemy and Flask-Bcrypt/app/__init__.py
@@ -0,0 +1,60 @@
+"""
+This is a boilerplate for doing a very simple session management app using 
+Flask-Login, Flask-SQLAlchemy and flask_bcrypt.
+
+Make sure to first those packages in the console with:
+    
+    'pip install flask-login flask-sqlalchemy flask_bcrypt'
+
+For a more traditional tutorial regarding the use of this packages see:
+ - https://www.sitepoint.com/flask-login-user-authentication/
+
+"""
+
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+DB_NAME = 'db.sqlite'
+db = SQLAlchemy()
+
+bcrypt = Bcrypt()
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'Very secret key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)
+
+    # Registering blueprints
+    from .auth import auth
+    from .main import main
+    app.register_blueprint(auth)
+    app.register_blueprint(main)
+
+    # Create db
+    from .models import User
+    create_database(app)
+
+    # Initialize the login manager
+    login_manager.init_app(app)
+    @login_manager.user_loader
+    def user_loader(id):
+        return db.session.get(User,int(id))
+
+    # Initialize bcrypt
+    bcrypt.init_app(app)
+
+    return app
+
+def create_database(app):
+    with app.app_context():
+        if not os.path.exists(f'instance/{DB_NAME}'):
+            db.create_all()
